ecc3754aaa319f37bdfe4764436fcacbe2c92bee
PranTanTheMan <46548127+PranTanTheMan@users.noreply.github.com>
2022-01-15 16:19:31 -0600

    yuh

--- src/game/play.java
+++ src/game/play.java
@@ -1,8 +1,20 @@
 package game;
 import java.util.*;
 import java.util.Random;
+import java.util.Scanner;
 
 
 public class play {
-    
+    Scanner reader = new Scanner(System.in);
+
+    void teamSelection(){
+        System.out.println("Choose the amount of overs from 1-3");
+        int decision = reader.nextInt();
+
+        if (decision == 1 || decision == 2 || decision == 3){
+            
+        }
+
+    }
+
 }
