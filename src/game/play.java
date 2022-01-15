c12c04bc6fd9ea6d62fd3edee2f470a31c7d75a3
PranTanTheMan <46548127+PranTanTheMan@users.noreply.github.com>
2022-01-15 16:49:00 -0600

    fuck me

--- src/game/play.java
+++ src/game/play.java
@@ -1,20 +1,29 @@
 package game;
-import java.util.*;
-import java.util.Random;
+// import java.util.*;
+// import java.util.Random;
 import java.util.Scanner;
 
 
 public class play {
     Scanner reader = new Scanner(System.in);
+    int overCount = 0;
 
-    void teamSelection(){
-        System.out.println("Choose the amount of overs from 1-3");
-        int overCount = reader.nextInt();
+    void Overs(){
+        System.out.println("Choose the amount of overs from 1-3.");
+        overCount = reader.nextInt();
 
-        if (overCount < 1){
-            
+        if (overCount < 1 || overCount > 3){
+            do {
+            System.out.println("Error. Please choose the correct amount of overs.");
+            overCount = reader.nextInt();
+            } while(overCount == 1 || overCount == 2 || overCount == 3);
         }
+    }
 
+    public static void main(String[] args) {
+        play method = new play();
+
+        method.Overs();
     }
 
 }
